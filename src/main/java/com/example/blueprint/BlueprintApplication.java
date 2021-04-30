a47662eb1e03505b972a59de7671e6e04b3181c4
Sebastian Kalinowski <sebkal1997@gmail.com>
2021-04-30 22:25:01 +0200

    feat: init

---
+++ src/main/java/com/example/blueprint/BlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blueprint/BlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
